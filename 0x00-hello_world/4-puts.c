8abd57af7fbd9fa82ecc2a4aca2be8b6c7f538f0
jamiu <jamiuahmed46@gmail.com>
2022-09-08 21:16:50 -0700

    corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ *A c code that outouts a text of string 
+ *object using the puts() function
+ */
 int main(){
-	 char my_char[100]= "Programming is like building a multilingual puzzle";
+	 char my_char[100]= "\"Programming is like building a multilingual puzzle"
+		 ;
 
 	puts(my_char);
 	return (0);
