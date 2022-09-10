ce4cc11c418969d731a2c5ff2316ccde527eb584
jamiu <jamiuahmed46@gmail.com>
2022-09-10 09:26:32 -0700

    still debugging

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,16 +12,15 @@
 int main(void)
 {
 	int x;
+
 	for (x = 'A'; x <= 'Z'; x++)
 	{
 		putchar(tolower(x));
 	}
-	
 	for (x = 'A'; x <= 'Z'; x++)
 	{
 		putchar(x);
 	}
 	putchar('\n');
-	
 	return (0);
 }
