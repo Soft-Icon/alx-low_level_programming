740650b4123bd8f899aeae5d430f28da31b06eb5
jamiu <jamiuahmed46@gmail.com>
2022-09-10 11:07:30 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * Description: main entry point
+ * init pointer  
+ *
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y =48; y < 58; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
+}
