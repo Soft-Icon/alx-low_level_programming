a54941273ceb0cd679eac22e966a6c65f228fc80
jamiu <jamiuahmed46@gmail.com>
2022-09-11 13:57:03 -0700

    Task Number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
- *A c code that outouts a text of string 
+ *Description:main - The main entry point
+ *
+ *A c code that outouts a text of string
  *object using the puts() function
+ * Return: Always return 0 (success)
  */
-int main(){
-	 char my_char[100]= "\"Programming is like building a multilingual puzzle"
-		 ;
+int main()
+{
+	 char my_char[100]= "\"Programming is like building a multilingual puzzle";
 
 	puts(my_char);
 	return (0);
