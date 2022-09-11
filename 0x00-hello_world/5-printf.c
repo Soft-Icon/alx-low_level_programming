ce42bd07d4ca726a532ac27546087b7a607912ea
jamiu <jamiuahmed46@gmail.com>
2022-09-11 14:51:24 -0700

    task number 5 done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Description:main - The main entry point
+ *some other codes goes in here
+ *
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	char x[100] = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s\n", x);
+	return (0);
+}
