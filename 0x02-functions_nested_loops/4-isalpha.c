ef668b29195bd5534d85e0eb3977db79d879806e
jamiu <jamiuahmed46@gmail.com>
2022-09-14 14:44:22 -0700

    my trial

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha -> function that checks for alphabet character
+ * @c: character argument
+ * Return: return 1 or 0
+ */
+int _isalpha(int c)
+{
+	return (c = 'a', 'A'; c <= 'z', 'Z'; c++);
+}
