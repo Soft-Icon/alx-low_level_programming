324b0c4111706856f0beb7c222a9747ad0943b01
jamiu <jamiuahmed46@gmail.com>
2022-09-19 18:06:55 -0700

    tried task 3

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,7 +7,7 @@
  */
 void swap_int(int *a, int *b)
 {
-	*a = b;
-	*b = a;
+	int *a = &b;
+	int *b = &a;
 
 }
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * int_strlen: this function returns the lenght of a string
+ * @s: the Pointer
+ * Return: always 0
+ */
+int _strlen(char *s)
+{
+	size_t strlen(const char *s);
+}
