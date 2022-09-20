6fdee4423c19511a568935af6da19d11e75dc837
jamiu <jamiuahmed46@gmail.com>
2022-09-19 21:14:13 -0700

    trial

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - this funcion prints sets of characters(strings) in reverse order
+ * @s: the pointer
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+	for (i = 0; i < 1; i++)
+	{
+		while (s[i] != '\0')
+			
+
