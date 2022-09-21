1b6258f14807f4d25c72873c70e0591791449643
jamiu <jamiuahmed46@gmail.com>
2022-09-20 21:15:31 -0700

    fixed

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <string.h>
 void reset_to_98(int *n);
@@ -12,4 +13,5 @@ void puts_half(char *str);
 int _putchar(char c);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 #endif
