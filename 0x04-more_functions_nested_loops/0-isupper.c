27838cc60d25ac00f6886741a4cef949b7501399
jamiu <jamiuahmed46@gmail.com>
2022-11-26 17:56:58 -0800

    taks number one

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code.
+ * @: _isupper
+ * return: Always 0.
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (c >= 'A' && c <= 'Z')
+		_putchar("1");
+	else 
+		_putchar("0");
+
+	return (0)
+}
