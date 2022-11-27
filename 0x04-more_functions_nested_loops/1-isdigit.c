2d5aa62007fdb2f2f08bf54a2deda56badf3aff0
jamiu <jamiuahmed46@gmail.com>
2022-11-26 18:42:14 -0800

    task number 2 of 7

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _isdigit -> checks for digits
+ * @c: character argument
+ *
+ * returns 1 if digit and 0 otherwise
+ */
+int _isdigit(int c)
+	return( c >= 0 && c <= 9);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
-
-int _putchar(char c)g
+int _isdigit(int c);
+int _putchar(char c);
 int _isupper(int c);
 #endif
